9bf20dbf5ee18290d8ed84cc01e41b6c52a9ad36
Jon Martin <jonmartin721@gmail.com>
2018-03-10 23:58:29 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    //Given the root to a binary tree, implement serialize(root), which serializes the tree into a string,
+    // and deserialize(s), which deserializes the string back into the tree.
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+
+
+}
